875f74b35c9d0bf95eceede91124e59a93d601dd
Manas Khurana <manas.khurana20@gmail.com>
2017-08-28 04:27:35 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+var CACHE_NAME = 'youcount-cache-1.0';
+var urlsToCache = [
+  '/index.html',
+  '/images/social.png',
+  '/js/script.js'
+];
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
