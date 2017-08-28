9064bbf43d6657f2b8d6e186037aec4093529b72
Manas Khurana <manas.khurana20@gmail.com>
2017-08-29 02:26:46 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+/* global noConnection:false */
 var CACHE_NAME = 'youcount-cache-1.0';
 var urlsToCache = [
   '/index.html',
@@ -8,20 +9,23 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function (cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+  try {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function (response) {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        })
+    );
+  } catch (err) {
+    noConnection('Service Worker failed to fetch!');
+  }
 });
