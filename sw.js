450db2fe4929d0c0caf07ae33d3491b3163ba800
Manas Khurana <manas.khurana20@gmail.com>
2017-08-30 01:04:09 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/* global noConnection:false */
+/* global noConnection:false, Promise:false */
 var CACHE_NAME = 'youcount-cache-1.2';
 var urlsToCache = [
   '/index.html',
@@ -13,7 +13,19 @@ self.addEventListener('install', function (event) {
       })
   );
 });
-
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+          return !(cacheName === CACHE_NAME);
+        }).map(function (cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
 self.addEventListener('fetch', function (event) {
   try {
     event.respondWith(
