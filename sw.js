bc08840c6588284ec6560f3f03944eab3ca323af
Manas Khurana <manas.khurana20@gmail.com>
2017-08-30 00:52:33 +0530

    Update and rename sw-v1.2.js to sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,24 @@
 /* global noConnection:false */
-var CACHE_NAME = 'youcount-cache-1.1';
+var CACHE_NAME = 'youcount-cache-1.2';
 var urlsToCache = [
   '/index.html',
   '/images/social.png',
   '/js/script.js'
 ];
-function retrieve(event) {
-  if (event) {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function (cache) {
-          return cache.addAll(urlsToCache);
-        })
-    );
-  } else {
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
-    });
-  }
-}
-self.addEventListener('install', retrieve(event));
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
 self.addEventListener('fetch', function (event) {
   try {
     event.respondWith(
       caches.match(event.request)
         .then(function (response) {
-          retrieve();
           if (response) {
             return response;
           }
