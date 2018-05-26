e47697dbc01b7a6398ca9086c369947795d99d2a
Manas Khurana <manas.khurana20@gmail.com>
2018-05-26 08:19:29 +0530

    Service Worker showing error: deferred
    Turns out it's a chrome bug: https://bugs.chromium.org/p/chromium/issues/detail?id=823392

--- sw.js
+++ sw.js
@@ -34,7 +34,7 @@ self.addEventListener('fetch', function (event) {
           if (response) {
             return response;
           }
-          return fetch(event.request, {mode: 'same-origin'});
+          return fetch(event.request);
         })
     );
   } catch (err) {
